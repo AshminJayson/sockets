5d3549278bcd583e8fde59b51807eac06bc9b76b
Ashmin Jayson <ashminjayson10@gmail.com>
2023-11-05 21:31:38 -0800

    chore: added vm files

---
+++ VM Java/Tcp.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.net.*;
+ 
+
+public class Tcp {
+ 
+    public static void main(String args[])
+    {
+        Client client = new Client("127.0.0.1", 5000);
+    }
+}
